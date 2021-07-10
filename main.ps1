0db36b565d97faea098cdcc099c0fc6f708840f6
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 21:39:46 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -45,7 +45,11 @@ if ("docker" -in $Install) {
       Write-Output "Checking"
       # sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "SELECT @@VERSION;"
       $sql = "SELECT 'np:\\.\pipe\' + CONVERT(NVARCHAR(128), SERVERPROPERTY('InstanceName')) + '\tsql\query' as servername"
-      sqlcmd -S "(localdb)\MSSQLLocalDB" -Q $sql | Select-Object -Last 1 -Skip 2
+      $sqlinstance = (sqlcmd -S "(localdb)\MSSQLLocalDB" -Q $sql | Select-Object -Last 1 -Skip 2).Trim()
+      reg add HKLM\SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo /v DSQUERY /d DBNETLIB /f
+      reg add HKLM\SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo /v localhost /d "DBNMPNTW,$sqlinstance" /f
+      
+      (sqlcmd -S "locahost" -Q $sql | Select-Object -Last 1 -Skip 2).Trim()
    }
 
    Write-Output "Waiting for docker to start"
