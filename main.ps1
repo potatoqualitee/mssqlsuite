c8447c3bbc6f19703ce0f9f9660967d8fa33e661
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 14:22:39 +0200

    update

--- main.ps1
+++ main.ps1
@@ -5,18 +5,11 @@ param (
 )
 Write-Output "$install"
 
-if ("sqlcmd" -in $Install) {
-   Write-Output "sqlcmd install"  
-   curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-   curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-   sudo apt-get update
-   sudo apt-get install mssql-tools unixodbc-dev
-   Write-Output 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-}
-
 if ("docker" -in $Install) {
    Write-Output "docker install"
    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+   Write-Output "Waiting for docker to start"
+   Start-Sleep -Seconds 5
 }
 
 if ("sqlpackage" -in $Install) {
