8795bae8b89ddf925466240cf954c3f4d468d278
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 16:44:19 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -12,6 +12,10 @@ if ("docker" -in $Install) {
       brew install docker docker-machine
       docker-machine create --driver virtualbox default
       docker-machine env default
+      # $profiledir = Split-Path $profile
+      if (-not (Test-Path $profiledir)) {
+         mkdir $profiledir
+      }
       docker-machine env default | Add-Content $profile
       docker-machine env default | Add-Content "$home/.bashrc"
       docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
