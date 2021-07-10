264b92c8c393850c55cefe9a03b9046c73f4a386
Chrissy LeMaire <clemaire@gmail.com>
2021-07-11 00:30:54 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -22,8 +22,8 @@ if ("docker" -in $Install) {
    if ($iswindows) {
       $ProgressPreference = "SilentlyContinue"
       Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-DEV-x64-ENU.exe -OutFile sqlsetup.exe
-
-      Start-Process -Wait -FilePath ./sqlsetup.exe -ArgumentList /qs, /extract:$PWD
+      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-DEV-x64-ENU.box -OutFile sqlsetup.box
+      Start-Process -Wait -FilePath ./sqlsetup.exe -ArgumentList /qs, /x:setup
       Get-ChildItem $PWD
       .\setup\setup.exe /q /ACTION=Install /INSTANCENAME=MSSQLSERVER /FEATURES=SQLEngine /UPDATEENABLED=0 /SQLSVCACCOUNT='NT AUTHORITY\NETWORK SERVICE' /SQLSYSADMINACCOUNTS='BUILTIN\ADMINISTRATORS' /TCPENABLED=1 /NPENABLED=0 /IACCEPTSQLSERVERLICENSETERMS
       
