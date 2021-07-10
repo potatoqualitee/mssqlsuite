aa13c6655a704097c97c24a90635dd893b8fa85e
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 23:46:44 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -8,7 +8,8 @@ if ("docker" -in $Install) {
    Write-Output "Installing docker"
    if ($ismacos) {
       brew install docker-machine docker
-      sudo docker –version
+      brew services start docker-machine
+      sudo docker –v
       docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
       Write-Output "Docker finished running"
       Start-Sleep 5
