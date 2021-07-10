495081391e40f3faf6ba144e8cb84123288b7b24
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 14:10:29 +0200

    replace with curl

--- main.ps1
+++ main.ps1
@@ -7,8 +7,8 @@ Write-Output "$install"
 
 if ("sqlcmd" -in $Install) {
    Write-Output "sqlcmd install"  
-   (Invoke-WebRequest https://packages.microsoft.com/keys/microsoft.asc).Content | sudo apt-key add -
-   (Invoke-WebRequest https://packages.microsoft.com/config/ubuntu/20.04/prod.list).Content | sudo tee /etc/apt/sources.list.d/msprod.list
+   curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+   curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
    sudo apt-get update
    sudo apt-get install mssql-tools unixodbc-dev
    Write-Output 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
