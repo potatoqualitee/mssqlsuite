0a7c09ad90516fd5857d6a01a37dd8102a36430a
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 14:03:23 +0200

    initial try

---
+++ main.ps1
@@ -0,0 +1,26 @@
+param (
+   [ValidateSet("sqlcmd","sqlpackage", "docker")]
+   [string[]]$Install,
+   [string]$SaPassword
+)
+Write-Output "$install"
+
+if ("sqlcmd" -in $Install) {
+   Write-Output "sqlcmd install"  
+   Invoke-WebRequest https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+   Invoke-WebRequest https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+   sudo apt-get update
+   sudo apt-get install mssql-tools unixodbc-dev
+   Write-Output 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+   source ~/.bashrc
+}
+
+if ("docker" -in $Install) {
+   Write-Output "docker install"
+   docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+}
+
+if ("sqlpackage" -in $Install) {
+   Write-Output "sqlpackage install"
+   Write-Output "jk"
+}
