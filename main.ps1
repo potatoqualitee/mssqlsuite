0d8d117345d47baf45928f93a8a25ff0ef5c79dc
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 15:28:09 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -7,9 +7,21 @@ Write-Output "$install"
 
 if ("docker" -in $Install) {
    Write-Output "docker install"
-   docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
-   Write-Output "Waiting for docker to start"
-   Start-Sleep -Seconds 5
+   if (-not $iswindows) {
+      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+      Write-Output "Waiting for docker to start"
+      Start-Sleep -Seconds 5
+   } else {
+      Get-Command wslbash
+      wslbash docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+      Write-Output "Waiting for docker to start"
+      Start-Sleep -Seconds 5
+   }
+}
+
+if ("sqlcmd" -in $Install) {
+   Write-Output "sqlcmd install"
+   
 }
 
 if ("sqlpackage" -in $Install) {
