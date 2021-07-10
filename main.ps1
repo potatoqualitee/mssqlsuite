47a2f9456655a6fead920382da88ba7be5f0cc2b
Chrissy LeMaire <clemaire@gmail.com>
2021-07-10 15:44:07 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -6,11 +6,24 @@ param (
 
 if ("docker" -in $Install) {
    Write-Output "docker install"
-   if (-not $iswindows) {
+   if ($ismacos) {
+      mkdir -p ~/.docker/machine/cache
+      curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
+      brew install docker docker-machine
+      docker-machine create --driver virtualbox default
+      docker-machine env default
       docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
       Write-Output "Waiting for docker to start"
       Start-Sleep -Seconds 5
-   } else {
+   }
+   
+   if ($islinux) {
+      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+      Write-Output "Waiting for docker to start"
+      Start-Sleep -Seconds 5
+   }
+
+   if ($iswindows) {
       bash whereis docker
       #bash docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
       Write-Output "Waiting for docker to start"
