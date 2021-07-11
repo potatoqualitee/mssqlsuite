c874b378c114d58d8783655e4d981d909d572494
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-11 13:35:37 +0200

    Initial release (#2)

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# sqlsuite
-A GitHub Action that automatically install SQL Server suite of tools
+# mssqlsuite
+A GitHub Action that automatically installs a SQL Server suite of tools including sqlcmd, bcp, sqlpackage, the sql engine, localdb and more for Windows, macOS and Linux.
+
+The `SqlServer` PowerShell module is included on the Windows runner. You can find more information about what's installed on GitHub runners on their [docs page](https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-software).
+
+## Documentation
+
+Just copy the code below and modify the line **`install: sqlengine, sqlclient, sqlpackage, localdb`** with the options you need.
+
+```yaml
+    - name: Install a SQL Server suite of tools
+      uses: potatoqualitee/mssqlsuite@v1
+      with:
+        install: sqlengine, sqlclient, sqlpackage, localdb
+```
+
+## Usage
+
+### Pre-requisites
+
+Create a workflow `.yml` file in your repositories `.github/workflows` directory. An [example workflow](#example-workflow) is available below. For more information, reference the GitHub Help Documentation for [Creating a workflow file](https://help.github.com/en/articles/configuring-a-workflow#creating-a-workflow-file).
+
+### Inputs
+
+* `install` - The apps to install. Options include: `sqlengine`, `sqlclient`, `sqlpackage`, and `localdb`
+* `sa_password` - The sa password for the SQL instance. The default is `dbatools.I0`
+* `show_log` - Show the log file for the docker container
+
+### Outputs
+
+None
+
+### Details
+
+| Application | Keyword | OS | Details | Time |
+| -------------- | ------------- | -------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------ | ------------- |
+| SQL Engine | sqlengine | Linux | Docker container with SQL Server 2019, accessible at `localhost` | ~45s |
+| SqlLocalDB | localdb | Linux | Not supported | N/A |
+| Client Tools | sqlclient | Linux | Already included in runner, including sqlcmd, bcp, and odbc drivers | N/A |
+| sqlpackage | sqlpackage | Linux | Installed from web | ~20s |
+| SQL Engine | sqlengine | Windows | Full install of SQL Server 2019, accessible at `localhost`. Docker took like 15 minutes. Windows and SQL authentication both supported. | ~5m |
+| SqlLocalDB | localdb | Windows | Accessible at `(localdb)\MSSQLLocalDB` | ~30s |
+| Client Tools | sqlclient | Windows | Already included in runner, including sqlcmd, bcp, and odbc drivers | N/A |
+| sqlpackage | sqlpackage | Windows | Installed using chocolatey | ~1.5m |
+| SQL Engine | sqlengine | macOS | Docker container with SQL Server 2019 running on VirtualBox, accessible at `localhost`. Docker [not supported on macOS](https://github.community/t/why-is-docker-not-installed-on-macos/17017) in GitHub Actions. | ~5m |
+| SqlLocalDB | localdb | macOS | Not supported | N/A |
+| Client Tools | sqlclient | macOS | Includes sqlcmd, bcp, and odbc drivers | ~2m |
+| sqlpackage | sqlpackage | macOS | Installed from web | ~25s |
+
+### Example workflows
+
+Installing everything on all OSes
+
+```yaml
+on: [push]
+
+jobs:
+  test-everywhere:
+    name: Test Action on all platforms
+    runs-on: ${{ matrix.os }}
+    strategy:
+      fail-fast: false
+      matrix:
+        os: [ubuntu-latest, windows-latest, macOS-latest]
+
+    steps:
+      - uses: actions/checkout@v2
+
+      - name: Run the action
+        uses: potatoqualitee/mssqlsuite@initial
+        with:
+          install: sqlengine, sqlclient, sqlpackage, localdb
+
+      - name: Run sqlclient
+        run: sqlcmd -S localhost -U sa -P dbatools.I0 -d tempdb -Q "SELECT @@version;"
+```
+
+## Contributing
+Pull requests are welcome!
+
+## TODO
+* Wait for GitHub Actions to support more stuff to make the install sleeker.
+* Maybe more tools from [here](https://docs.microsoft.com/en-us/sql/tools/sqlpackage/sqlpackage-download?view=sql-server-ver15).
+  * mssql-cli (command-line query tool)
+  * osql
+  * Profiler Utility
+  * sqlagent90 Application
+  * SQLdiag
+  * sqlmaint
+  * sqllogship Application
+  * sqlservr Application
+  * tablediff
+
+## License
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
+
---
+++ main.ps1
@@ -0,0 +1,148 @@
+param (
+   [ValidateSet("sqlclient","sqlpackage", "sqlengine", "localdb")]
+   [string[]]$Install,
+   [string]$SaPassword,
+   [switch]$ShowLog
+)
+
+if ("sqlengine" -in $Install) {
+   Write-Output "Installing SQL Engine"
+   if ($ismacos) {
+      Write-Output "mac detected, installing docker then downloading a docker container"
+      mkdir -p ~/.docker/machine/cache
+      curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
+      brew install docker docker-machine
+      docker-machine create --driver virtualbox --virtualbox-memory 3072 default
+      docker-machine env default
+      
+      $profiledir = Split-Path $profile
+      if (-not (Test-Path $profiledir)) {
+         mkdir $profiledir
+         "" | Add-Content $profile
+      }
+
+      docker-machine env default | Add-Content "$home/.bashrc"
+      docker-machine env default | Add-Content $profile
+      ((Get-Content $profile) -replace 'export ','$env:') | Set-Content $profile
+      . $profile
+      docker-machine stop default
+      VBoxManage modifyvm "default" --natpf1 "mssql,tcp,,1433,,1433"
+      docker-machine start default
+      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" --name sql -p 1433:1433 --memory="2g" -d mcr.microsoft.com/mssql/server:2019-latest
+      Write-Output "Docker finished running"
+      Start-Sleep 5
+      if ($ShowLog) {
+         docker-machine ip default
+         docker ps -a
+         docker-machine ip
+         docker-machine ls
+         docker logs -t sql
+      }
+      
+      Write-Output "sql engine installed at localhost"
+   }
+
+   if ($islinux) {
+      Write-Output "linux detected, downloading the 2019 docker container"
+      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" --name sql -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+      Write-Output "Waiting for docker to start"
+      Start-Sleep -Seconds 10
+      
+      if ($ShowLog) {
+         docker ps -a
+         docker-machine ip
+         docker-machine ls
+         docker logs -t sql
+      }
+      Write-Output "docker container running - sql server accessible at localhost"
+   }
+
+   if ($iswindows) {
+      Write-Output "windows detected, downloading sql server 2019"
+      # docker takes 16 minutes, this takes 5 minutes
+      if (-not (Test-Path C:\temp)) {
+         mkdir C:\temp
+      }
+      Push-Location C:\temp
+      $ProgressPreference = "SilentlyContinue"
+      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-DEV-x64-ENU.exe -OutFile sqlsetup.exe
+      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-DEV-x64-ENU.box -OutFile sqlsetup.box
+      Start-Process -Wait -FilePath ./sqlsetup.exe -ArgumentList /qs, /x:setup
+      .\setup\setup.exe /q /ACTION=Install /INSTANCENAME=MSSQLSERVER /FEATURES=SQLEngine /UPDATEENABLED=0 /SQLSVCACCOUNT='NT AUTHORITY\NETWORK SERVICE' /SQLSYSADMINACCOUNTS='BUILTIN\ADMINISTRATORS' /TCPENABLED=1 /NPENABLED=0 /IACCEPTSQLSERVERLICENSETERMS
+      Set-ItemProperty -path 'HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQLSERVER\' -Name LoginMode -Value 2 
+      Restart-Service MSSQLSERVER
+      sqlcmd -S localhost -q "ALTER LOGIN [sa] WITH PASSWORD=N'$SaPassword'"
+      sqlcmd -S localhost -q "ALTER LOGIN [sa] ENABLE"
+      Pop-Location
+      
+      Write-Output "sql server 2019 installed at localhost and accessible with both windows and sql auth"
+   }
+}
+
+if ("sqlclient" -in $Install) {
+   if ($ismacos) {
+      Write-Output "Installing sqlclient tools"
+      brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
+      $null = brew update
+      $log = brew install msodbcsql17 mssql-tools
+      
+      if ($ShowLog) {
+         $log
+      }
+   }
+   
+   Write-Output "sqlclient tools are installed"
+}
+
+if ("sqlpackage" -in $Install) {
+   Write-Output "installing sqlpackage"
+
+   if ($ismacos) {
+      curl "https://go.microsoft.com/fwlink/?linkid=2143659" -4 -sL -o '/tmp/sqlpackage.zip'
+      $log = unzip /tmp/sqlpackage.zip -d $HOME/sqlpackage
+      chmod +x $HOME/sqlpackage/sqlpackage
+      sudo ln -sf $HOME/sqlpackage/sqlpackage /usr/local/bin
+      if ($ShowLog) {
+         $log
+         sqlpackage /version
+      }
+   }
+
+   if ($islinux) {
+      curl "https://go.microsoft.com/fwlink/?linkid=2143497" -4 -sL -o '/tmp/sqlpackage.zip'
+      $log = unzip /tmp/sqlpackage.zip -d $HOME/sqlpackage
+      chmod +x $HOME/sqlpackage/sqlpackage
+      sudo ln -sf $HOME/sqlpackage/sqlpackage /usr/local/bin
+      if ($ShowLog) {
+         $log
+         sqlpackage /version
+      }
+   }
+
+   if ($iswindows) {
+      $log = choco install sqlpackage
+      if ($ShowLog) {
+         $log
+         sqlpackage /version
+      }
+   }
+   
+   Write-Output "sqlpackage installed"
+}
+
+if ("localdb" -in $Install) {
+   if ($iswindows) {
+      Write-Host "Downloading SqlLocalDB"
+      $ProgressPreference = "SilentlyContinue"
+      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SqlLocalDB.msi -OutFile SqlLocalDB.msi
+      Write-Host "Installing"
+      Start-Process -FilePath "SqlLocalDB.msi" -Wait -ArgumentList "/qn", "/norestart", "/l*v SqlLocalDBInstall.log", "IACCEPTSQLLOCALDBLICENSETERMS=YES";
+      Write-Host "Checking"
+      sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "SELECT @@VERSION;"
+      sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "ALTER LOGIN [sa] WITH PASSWORD=N'$SaPassword'"
+      
+      Write-Host "SqlLocalDB installed and accessible at (localdb)\MSSQLLocalDB"
+   } else {
+      Write-Output "localdb cannot be isntalled on mac or linux"
+   }
+}
