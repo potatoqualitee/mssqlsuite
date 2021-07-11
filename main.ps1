42ad76707c25f40f78a2b7c32a0c5e6cba86e8f1
Chrissy LeMaire <clemaire@gmail.com>
2021-07-11 08:52:49 +0200

    maybe this works except mac

--- main.ps1
+++ main.ps1
@@ -4,7 +4,7 @@ param (
    [string]$SaPassword
 )
 
-if ("docker" -in $Install) {
+if ("engine" -in $Install) {
    Write-Output "Installing docker"
    if ($ismacos) {
       
@@ -25,7 +25,7 @@ if ("docker" -in $Install) {
       . $profile
       docker-machine ip default
       Start-Sleep 5
-      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server
+      docker run -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
       Write-Output "Docker finished running"
       docker-machine ssh default -L 1433:localhost:1433
       Start-Sleep 5
@@ -35,16 +35,28 @@ if ("docker" -in $Install) {
    }
 
    if ($islinux) {
-      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+      docker run -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+      Write-Output "Waiting for docker to start"
+      Start-Sleep -Seconds 10
    }
 
    if ($iswindows) {
-      docker pull microsoft/mssql-server-windows-developer --disable-content-trust
-      #docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -p 1433:1433 -d microsoft/mssql-server-windows-developer
+      # docker takes 16 minutes, this takes 5 minutes
+      if (-not (Test-Path C:\temp)) {
+         mkdir C:\temp
+      }
+      Push-Location C:\temp
+      $ProgressPreference = "SilentlyContinue"
+      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-DEV-x64-ENU.exe -OutFile sqlsetup.exe
+      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-DEV-x64-ENU.box -OutFile sqlsetup.box
+      Start-Process -Wait -FilePath ./sqlsetup.exe -ArgumentList /qs, /x:setup
+      Get-ChildItem $PWD
+      .\setup\setup.exe /q /ACTION=Install /INSTANCENAME=MSSQLSERVER /FEATURES=SQLEngine /UPDATEENABLED=0 /SQLSVCACCOUNT='NT AUTHORITY\NETWORK SERVICE' /SQLSYSADMINACCOUNTS='BUILTIN\ADMINISTRATORS' /TCPENABLED=1 /NPENABLED=0 /IACCEPTSQLSERVERLICENSETERMS
+      Set-ItemProperty -path 'HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQLSERVER\' -Name LoginMode -Value 2 
+      Restart-Service MSSQLSERVER
+      sqlcmd -S localhost -q "ALTER LOGIN [sa] WITH PASSWORD=N'$SaPassword'"
+      Pop-Location
    }
-
-   Write-Output "Waiting for docker to start"
-   Start-Sleep -Seconds 10
 }
 
 if ("sqlcmd" -in $Install) {
@@ -52,8 +64,6 @@ if ("sqlcmd" -in $Install) {
 
    if ($ismacos) {
       brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
-      #brew update
-      $env:ACCEPT_EULA = 'Y'
       brew install msodbcsql17 mssql-tools
    }
    
@@ -87,12 +97,13 @@ if ("sqlpackage" -in $Install) {
 if ("localdb" -in $Install) {
    if ($iswindows) {
       Write-Host "Downloading"
-      Import-Module BitsTransfer
-      Start-BitsTransfer -Source https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SqlLocalDB.msi -Destination SqlLocalDB.msi
+      $ProgressPreference = "SilentlyContinue"
+      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SqlLocalDB.msi -OutFile SqlLocalDB.msi
       Write-Host "Installing"
       Start-Process -FilePath "SqlLocalDB.msi" -Wait -ArgumentList "/qn", "/norestart", "/l*v SqlLocalDBInstall.log", "IACCEPTSQLLOCALDBLICENSETERMS=YES";
       Write-Host "Checking"
-      sqlcmd -l 60 -S "(localdb)\MSSQLLocalDB" -Q "SELECT @@VERSION;"
+      sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "SELECT @@VERSION;"
+      sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "ALTER LOGIN [sa] WITH PASSWORD=N'$SaPassword'"
    } else {
       Write-Output "localdb cannot be isntalled on mac or linux"
    }
